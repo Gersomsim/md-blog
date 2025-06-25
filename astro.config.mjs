34ecbebacd31b9de349d4926f7e8f86a29771190
Gersomsim <gersomhernandez@me.com>
2025-06-25 13:21:31 -0600

    archivos de configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,24 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  integrations: [
+    mdx()
+  ],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      langs: []
+    },
+    syntaxHighlight: 'shiki'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
